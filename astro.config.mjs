8fb423cc14ac8dc769c89029a1b15f9577d66fa7
Adarsh Jaiswal <adarshjaiswal1989@gmail.com>
2026-04-20 11:42:49 +0530

    corrected name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://heluvaguy.github.io',
   base: '/invisible-interface',
   integrations: [mdx()],
 });
